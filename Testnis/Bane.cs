1882c4807cbec882939894a81b2a527052792322
VixonyAn <vixonyan@gmail.com>
2025-04-10 13:12:08 +0200

    Klasser og funktioner oprettet

---
+++ Testnis/Bane.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testnis
+{
+    public class Bane
+    {
+        #region Properties
+        public int BaneID { get; set; } // 1, 2, 3
+        public string BaneType { get; set; } // tennis / padel
+        public int BaneStr { get; set; } // størrelse
+        public DateTime SidstOrdnet { get; set; }
+        #endregion
+
+        public Bane() { }
+        public Bane(int baneId, string baneType, int baneStr)
+        {
+            BaneID = baneId;
+            BaneType = baneType;
+            BaneStr = baneStr;
+            SidstOrdnet = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return $"Bane ID: {BaneID}\nBane Type: {BaneType}\nBane Størrelse: {BaneStr}\nSidst Ordnet: {SidstOrdnet}";
+        }
+    }
+}
---
+++ Testnis/BaneRepo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testnis
+{
+    public class BaneRepo : IBaneRepo
+    {
+        #region Instance Fields
+        private Dictionary<int, Bane> _baner;
+        #endregion
+
+        #region Constructors
+        public BaneRepo()
+        {
+            _baner = new Dictionary<int, Bane>();
+        }
+        #endregion
+
+        #region Methods
+        public List<Bane> GetAllBane()
+        {
+            return _baner.Values.ToList();
+        }
+        public Bane GetBaneById(int baneId)
+        {
+            if (baneId != null && _baner.ContainsKey(baneId))
+            {
+                return _baner[baneId];
+            }
+            return null;
+        }
+
+        public void CreateBane(Bane bane)
+        {
+            if (!_baner.ContainsKey(bane.BaneID)) // hvis key er ledigt
+            {
+                _baner.Add(bane.BaneID, bane);
+            }
+            else
+            {
+                throw new Exception($"Bane ID '{bane.BaneID}' findes allerede\n");
+            }
+        }
+        public void UpdateBane(Bane bane, int baneId)
+        {
+            if (_baner.ContainsKey(baneId)) // hvis key ikke er ledigt
+            {
+                throw new Exception($"Bane ID '{bane.BaneID}' findes allerede\n");
+            }
+            if (_baner[baneId].BaneID != baneId)
+            {
+                throw new Exception($"Violation of primary key, this value may not be changed");
+            }
+            _baner[baneId] = bane;
+        }
+
+        public void DeleteBane(int baneId)
+        {
+            _baner.Remove(baneId);
+        }
+        #endregion
+    }
+}
